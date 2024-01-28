6928a68ac90cfd4e1f61774b4de5190a17da38df
Nolan <nbfl9@icloud.com>
2024-01-27 23:08:35 -0600

    update

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector('.Carta').addEventListener('click', () => {
+    document.querySelector('.Letter').classList.toggle('show');
+});  
+
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.Letter').classList.toggle('show');
+});
